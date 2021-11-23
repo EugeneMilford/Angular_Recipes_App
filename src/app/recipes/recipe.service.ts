fca9b779bacf0ba2c939468bf39376253a2e4a11
Eugene Milford <gmilford321@gmail.com>
2021-11-23 20:41:57 +0200

    Recipes-App service update

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,41 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.module";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test recipe', 'desc = this is a test', 'https://www.grizzlybearmodern.com/wp-content/uploads/2020/01/fdddd.jpeg'),
-    new Recipe('Test recipe', 'desc = this is another test', 'https://www.grizzlybearmodern.com/wp-content/uploads/2020/01/fdddd.jpeg')
+    new Recipe(
+      'Delicious Fish',
+      'desc = Home made style fish',
+      'https://img.etimg.com/thumb/msid-68495044,width-650,imgsize-1335371,,resizemode-4,quality-100/fish.jpg',
+      [
+        new Ingredient('Fish', 2),
+        new Ingredient('Lemons', 2)
+      ]),
+
+    new Recipe(
+      'Cheese Burger',
+      'desc = This is a recipe from epicurious',
+      'https://assets.epicurious.com/photos/5c745a108918ee7ab68daf79/1:1/w_1920,c_limit/Smashburger-recipe-120219.jpg',
+      [
+      new Ingredient('Meat', 1),
+      new Ingredient('Buns', 2)
+      ])
   ];
+
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice();
   }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.AddIngs(ingredients);
+  }
+
 }
