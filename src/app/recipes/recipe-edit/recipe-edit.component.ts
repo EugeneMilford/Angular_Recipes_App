d2407975d8b47a24fa8579dba6c982902784c63a
Eugene Milford <gmilford321@gmail.com>
2021-11-30 18:26:22 +0200

    Recipes-App_Forms_Completed
    The last modifications to the forms for a new recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -100,4 +100,9 @@ export class RecipeEditComponent implements OnInit {
     this.router.navigate(['../'], {relativeTo: this.route});
   }
 
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+    /*(<FormArray>this.recipeForm.get('ingredients')).clear();*/
+  }
+
 }
