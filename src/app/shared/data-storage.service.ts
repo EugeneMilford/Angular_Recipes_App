eaf211fdceef38a3f69d805ea8d9d05b0fc35349
Eugene Milford <gmilford321@gmail.com>
2021-12-01 21:04:53 +0200

    HTTP Requests final
    Final changes to HTTP made

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -18,10 +19,18 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>('https://angular-recipe-book-28701-default-rtdb.firebaseio.com/recipes.json')
-      .subscribe(recipes => {
-        this.recipeService.setRecipes(recipes);
-    });
+    return this.http.get<Recipe[]>('https://angular-recipe-book-28701-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+    )
   }
 
 }
