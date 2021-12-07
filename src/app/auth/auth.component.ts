b1bfcb2be59bdd863e2306b832c4e980916145ae
Eugene Milford <gmilford321@gmail.com>
2021-12-07 11:07:09 +0200

    Authentication added
    Authentication and firebase connection added to project

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html'
+})
+export class AuthComponent {
+
+  isLoggedIn = true;
+
+  onSwitchMode() {
+    this.isLoggedIn = !this.isLoggedIn;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
+}
