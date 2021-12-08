583ec19a52ac03c9d0e8590abde8efe60c6ed74f
Eugene Milford <gmilford321@gmail.com>
2021-12-08 12:30:24 +0200

    Loading spinner added
    A loading spinner and an error message added

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -7,14 +8,41 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent {
 
+  constructor(private aService: AuthService) { }
+
   isLoggedIn = true;
+
+  isLoading = false;
+
+  error: string = null;
 
   onSwitchMode() {
     this.isLoggedIn = !this.isLoggedIn;
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.isLoading = true;
+
+    if (this.isLoggedIn) {
+
+    } else {
+      this.aService.signUp(email, password).subscribe(resData => {
+        console.log(resData);
+        this.isLoading = false;
+      }, error => {
+        console.log(error);
+        this.error = 'An error has occurred...';
+        this.isLoading = false;
+      });
+    }
+
     form.reset();
   }
 }
