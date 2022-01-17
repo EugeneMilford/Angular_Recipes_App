96e77617745858bbf874cd7d2674daca38a68ed4
Eugene Milford <gmilford321@gmail.com>
2022-01-17 12:30:30 +0200

    Auth guard

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
@@ -6,9 +8,19 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html'
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor(private dsService: DataStorageService) { }
+  isAuthenticated = false;
+
+  private userSub: Subscription;
+
+  constructor(private dsService: DataStorageService, private authservice: AuthService) { }
+
+  ngOnInit() {
+    this.userSub = this.authservice.user.subscribe(user => {
+      this.isAuthenticated = !!user; /*!user ? false : true;*/
+    });
+  }
 
   onSaveData() {
     this.dsService.storeRecipes();
@@ -16,5 +28,14 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dsService.fetchRecipes().subscribe();;
+  }
+
+  onLogout()
+  {
+    this.authservice.logout();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
